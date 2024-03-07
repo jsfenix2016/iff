b0bfd5541fe7fbb556cc74dad140c139d4c16712
Tania Vivanco <tania.vivanco@sppanel.es>
2024-03-06 23:49:46 -0600

    Se corrige el refresh de alertas cuando le das cancelar y se ajusta el metodo que filtra las alertas repetidas

--- alonefy_app/alonefy_app/lib/Page/Alerts/PageView/alerts_page.dart
+++ alonefy_app/alonefy_app/lib/Page/Alerts/PageView/alerts_page.dart
@@ -1,12 +1,19 @@
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:ifeelefine/Common/decoration_custom.dart';
+import 'package:ifeelefine/Common/initialize_models_bd.dart';
 import 'package:ifeelefine/Common/notificationService.dart';
 import 'package:ifeelefine/Common/text_style_font.dart';
 import 'package:ifeelefine/Common/utils.dart';
+import 'package:ifeelefine/Data/hiveRisk_data.dart';
+import 'package:ifeelefine/Model/contactRiskBD.dart';
 import 'package:ifeelefine/Model/logAlertsBD.dart';
 import 'package:ifeelefine/Page/Alerts/Controller/alertsController.dart';
 import 'package:ifeelefine/Page/Alerts/Widget/alert_list_group_widget.dart';
+import 'package:ifeelefine/Page/Risk/DateRisk/Controller/editRiskController.dart';
+import 'package:ifeelefine/Page/Risk/DateRisk/ListDateRisk/Controller/riskPageController.dart';
+import 'package:ifeelefine/Provider/prefencesUser.dart';
+import 'package:ifeelefine/Services/mainService.dart';
 import 'package:ifeelefine/Utils/Widgets/loading_page.dart';
 import 'package:ifeelefine/main.dart';
 
@@ -18,6 +25,7 @@ class AlertsPage extends StatefulWidget {
 }
 
 class _AlertsPageState extends State<AlertsPage> {
+  final PreferenceUser _prefs = PreferenceUser();
   final AlertsController alertsVC = Get.put(AlertsController());
 
   final scaffoldKey = GlobalKey<ScaffoldState>();
@@ -44,6 +52,76 @@ class _AlertsPageState extends State<AlertsPage> {
       getLog();
     }
   }
+
+  Future<void> onRefresh(bool value) async{
+    alertsVC.update();
+    mainController.refreshHome();
+    setState(() {});
+  }
+
+  Future<void> cancelNotify() async {
+    List<String> listTaskIds = _prefs.getlistTaskIdsCancel;
+    if (listTaskIds.isEmpty) {
+      listTaskIds = getTaskIdList(prefs.getCancelIdDate.toString());
+    } else {
+      listTaskIds = getTaskIdList(listTaskIds.first);
+    }
+
+    if (_prefs.getNotificationType.toString().contains('Cita')) {
+      var contactRisk = await const HiveDataRisk().getcontactRiskbd;
+      if (listTaskIds.isEmpty) {
+        EditRiskController erisk = Get.put(EditRiskController());
+        RiskController risk = Get.put(RiskController());
+        var resp = await risk.getContactsRisk();
+        ContactRiskBD tempcontact = initContactRisk();
+        for (var temp in resp) {
+          DateTime starTime = parseContactRiskDate(temp.timeinit);
+          bool isafter = DateTime.now().isAfter(starTime);
+          if (!temp.isActived &&
+              temp.isprogrammed &&
+              isafter &&
+              temp.isFinishTime == false) {
+            tempcontact = temp;
+          }
+        }
+
+        var contactRiskTemp = tempcontact;
+
+        if (contactRiskTemp.id != -1) {
+          await erisk.updateContactRisk(contactRiskTemp);
+        }
+
+        return;
+      }
+      _prefs.saveLastScreenRoute("cancelDate");
+      for (var element in contactRisk) {
+        if (element.isActived || element.isFinishTime) {
+          RedirectViewNotifier.onTapNotification(listTaskIds, (element.id));
+        }
+      }
+      onRefresh(true);
+      getLog();
+      return;
+    }
+    if (_prefs.getNotificationType.toString().contains('Inactividad')) {
+      await mainController.saveUserLog("Inactividad - Actividad detectada ",
+          DateTime.now(), _prefs.getIdInactiveGroup);
+    }
+    if (_prefs.getNotificationType.toString().contains('Caida')) {
+      await mainController.saveUserLog("Caida - Actividad detectada ", DateTime.now(),
+          _prefs.getIdDropGroup);
+    }
+    _prefs.setEnableTimer = false;
+    _prefs.setEnableTimerDrop = false;
+    MainService().cancelAllNotifications(listTaskIds);
+    _prefs.setNotificationType = "";
+    // typeNotify = "";
+    await flutterLocalNotificationsPlugin.cancel(_prefs.getNotificationId!);
+    _prefs.setNotificationId = -1;
+    onRefresh(true);
+    getLog();
+  }
+
 
   @override
   void initState() {
@@ -93,11 +171,8 @@ class _AlertsPageState extends State<AlertsPage> {
                       onChangedDelete: (List<LogAlertsBD> value) {
                         deleteForDayMov(context, value);
                       },
-                      onRefresh: (bool value) {
-                        alertsVC.update();
-                        mainController.refreshHome();
-
-                        setState(() {});
+                      onCancelNotification: (value) {
+                        cancelNotify();
                       },
                     );
                   }),
